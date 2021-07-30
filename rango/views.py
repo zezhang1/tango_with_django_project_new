d3fccb25a3de43c4008b8920d8219111880e2cdf
Ze Zhang <2544789z@student.gla.ac.uk>
2021-07-30 16:26:42 +0800

    chapter8 second commit

--- rango/views.py
+++ rango/views.py
@@ -62,7 +62,7 @@ def add_category(request):
 
         if form.is_valid():
             form.save(commit=True)
-            return redirect('/rango/')
+            return redirect(reverse('rango:index'))
         else:
             print(form.errors)
 
@@ -76,7 +76,7 @@ def add_page(request, category_name_slug):
         category = None
 
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
 
     form = PageForm()
 
